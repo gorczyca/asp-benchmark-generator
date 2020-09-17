d25a4c28a55bb6bd2ea8dbe7e8e26cdf4b6ecc54
gorczyca <gorczycapj@gmail.com>
2020-09-17 19:30:33 +0200

    version 1.0

--- project_info.py
+++ project_info.py
@@ -5,7 +5,7 @@
 
 PROJECT_NAME = 'BenchmarkGenerator'
 PROJECT_DESCRIPTION = "Benchmark generator of configuration's problem ASP encodings"
-PROJECT_VERSION = '0.1'
+PROJECT_VERSION = '1.0'
 PROJECT_WEBSITE = 'https://github.com/gorczyca/asp-benchmark-generator'
 AUTHOR = 'Piotr Gorczyca'
 AUTHOR_EMAIL = 'gorczycapj@gmail.com'
